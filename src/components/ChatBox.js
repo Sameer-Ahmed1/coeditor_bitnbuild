da852aa4b7dd412102456919d1079e47ee75e012
shivaramakrishnamarepalli <marepallishivaramakrishna@gmail.com>
2024-02-07 23:53:54 +0530

    chatbox version1

--- src/components/ChatBox.js
+++ src/components/ChatBox.js
@@ -1,8 +1,19 @@
 import "./chatBox.css"
-function ChatBox(){
-    return (<div className="ChatBox">
-        ChatBox component
-    </div>)
+
+function ChatBox({ chat }) {
+    return (
+        <div className="ChatBox">
+            {chat === null ? (
+                <p>No messages</p>
+            ) : (
+                <ul>
+                    {chat.map((message, index) => (
+                        <li key={index}>{message}</li>
+                    ))}
+                </ul>
+            )}
+        </div>
+    );
 }
 
 export default ChatBox;
