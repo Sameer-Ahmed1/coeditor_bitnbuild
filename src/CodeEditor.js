9cc43998f5a123a760f00e4c475ca6dc9723a167
Sameer Ahmed <sameernadeemi1111@gmail.com>
2024-02-08 02:47:07 +0530

    added multi file editing

--- src/CodeEditor.js
+++ src/CodeEditor.js
@@ -1,26 +1,99 @@
 import { Editor } from "@monaco-editor/react";
 import { useEffect, useState } from "react";
+import socketIOClient from "socket.io-client";
+const ENDPOINT = "http://localhost:4001";
 
 export default function CodeEditor() {
-  const [code, setCode] = useState("// write your code here");
+  const DEFAULT_CODE = "// write your code here";
+  const DEFAULT_FILE_NAME = "script.js";
+
+  const [socket, setSocket] = useState(null);
+  const [files, setFiles] = useState({
+    [DEFAULT_FILE_NAME]: DEFAULT_CODE,
+  });
   const [isReadOnly, setIsReadOnly] = useState(false);
+  const [roomId, setRoomId] = useState("");
+  const [activeFile, setActiveFile] = useState("script.js");
+
+  useEffect(() => {
+    const newSocket = socketIOClient(ENDPOINT);
+    setSocket(newSocket);
+
+    return () => {
+      newSocket.disconnect();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (socket == null) return;
+    socket.on("code", ({ fileName, newCode }) => {
+      setFiles((oldFiles) => ({
+        ...oldFiles,
+        [fileName]: newCode,
+      }));
+    });
+  }, [socket]);
+
+  const handleCodeChange = (newCode) => {
+    setFiles((oldFiles) => ({
+      ...oldFiles,
+      [activeFile]: newCode,
+    }));
+    if (socket) {
+      socket.emit("code", { roomId: roomId, fileName: activeFile, newCode });
+    }
+  };
+
+  const joinRoom = () => {
+    if (socket == null) return;
+    socket.emit("join", roomId);
+  };
+  const leaveRoom = () => {
+    if (socket == null) return;
+    socket.emit("leave", roomId);
+    setRoomId(""); // Clear the room ID
+  };
+
   const toggleReadOnly = () => {
     setIsReadOnly(!isReadOnly);
   };
 
-  useEffect(() => {
-    console.log(code);
-  }, [code]);
   return (
     <div>
+      <button
+        disabled={activeFile === "script.js"}
+        onClick={() => setActiveFile("script.js")}
+      >
+        script.js
+      </button>
+      <button
+        disabled={activeFile === "style.css"}
+        onClick={() => setActiveFile("style.css")}
+      >
+        style.css
+      </button>
+      <button
+        disabled={activeFile === "index.html"}
+        onClick={() => setActiveFile("index.html")}
+      >
+        index.html
+      </button>
+      <input
+        type="text"
+        value={roomId}
+        onChange={(e) => setRoomId(e.target.value)}
+        placeholder="Enter room ID"
+      />
+      <button onClick={joinRoom}>Join Room</button>
+      <button onClick={leaveRoom}>Leave Room</button>
       <div className={` ${isReadOnly ? "disable-input" : ""}`}>
-      <p>code editor</p>
+        <p>code editor : {activeFile}</p>
         <Editor
           height="90vh"
           defaultLanguage="javascript"
           defaultValue="// some comment"
-          value={code}
-          onChange={(value) => setCode(value)}
+          value={files[activeFile]?.toString() || DEFAULT_CODE}
+          onChange={handleCodeChange}
         />
       </div>
       <button onClick={toggleReadOnly}>
