a70ee9afe25ad1220a90811e7633cf6fcb5e654a
PraneethReddy15 <ksaipraneethreddy123@gmail.com>
2024-02-09 23:16:39 +0530

    chatbox codebox fixed

--- src/components/Coeditor.js
+++ src/components/Coeditor.js
@@ -35,13 +35,13 @@ export default function Coeditor({
       </div>
       {isCardVisible && <div className={`code-card`}>
         <div className="input">
-          <input placeholder="Enter input here"/>
+          <input className="input-text" placeholder="Enter input here"/>
         </div>
         <div className="output">
           <pre>{fileContents}</pre>
         </div>
         <button onClick={closeCard} className="close-button">
-          Close
+        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M6.4 19L5 17.6l5.6-5.6L5 6.4L6.4 5l5.6 5.6L17.6 5L19 6.4L13.4 12l5.6 5.6l-1.4 1.4l-5.6-5.6z"/></svg>
         </button>
       </div>}
       <Editor
